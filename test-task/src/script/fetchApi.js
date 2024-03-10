0a2ae7f6c5ab266899e92de7efa590ab28fa40af
kirka50 <75656501+kirka50@users.noreply.github.com>
2024-03-10 21:53:04 +0500

    done

--- test-task/src/script/fetchApi.js
+++ test-task/src/script/fetchApi.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import createMD5 from "./createMD5.js";
 
 export default async function fetchApi(action, params = null) {
-    return await axios.post('http://api.valantis.store:40000/', {
+    return await axios.post('https://api.valantis.store:41000/', {
         action: action,
         ...(params? {params: params} : {})
     },
